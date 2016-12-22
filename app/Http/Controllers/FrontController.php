803b8dc4a68d9c18309dda563e91d4050cff5b0d
exopole <alexandre.nicaise28@gmail.com>
2016-12-22 21:47:00 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   .env.example
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   .phpintel/100e7953b5d1aae1ee451a18deaacf90
    	new file:   .phpintel/125304f958fd7c902b1f5aa952a76d03
    	new file:   .phpintel/1c0cbcd9f9eb5948c9fa77ca168e8e8b
    	new file:   .phpintel/2d9ea8f30ff9fb8c5da1dd1b7b24da31
    	new file:   .phpintel/306abbde73ff99fd52f45842caed5b7f
    	new file:   .phpintel/4435749681de58208baf1f4d2ff15113
    	new file:   .phpintel/4b811e04153207f2519021500a67ad37
    	new file:   .phpintel/548683afc40bf53d0eabf69f673651be
    	new file:   .phpintel/558c09b4732855fe491c30217ba6f5d1
    	new file:   .phpintel/584c0dc55be7cb878aa469208f9a8109
    	new file:   .phpintel/6a30b8d1e3193ba541de7615f11762a1
    	new file:   .phpintel/87a8005de72f4b236d051dadf35295f8
    	new file:   .phpintel/88a7aa0ddc8172343695cd695d7e4cc0
    	new file:   .phpintel/a2771bc8bc539d13ba24c3dd6ac43073
    	new file:   .phpintel/a350deec34a9dfa0ee4c375e4133c01a
    	new file:   .phpintel/b5366e1658ec4847451e4e7d0c44e3f9
    	new file:   .phpintel/beefb64ce15e4811b70efaf48db520ab
    	new file:   .phpintel/c0dd97ef35cd41f4f087a6a667841651
    	new file:   .phpintel/cbb853101370f4a08044f282c539ed4e
    	new file:   .phpintel/ce583d19249dc64f174b8656f6936870
    	new file:   .phpintel/d3c8b3f5da10fbd7028b52a6a7cbe044
    	new file:   .phpintel/d6a7814bb475581928f81b5245633d59
    	new file:   .phpintel/d6e25565e1087350c800530b1a1d0e6a
    	new file:   .phpintel/de4c98ca2fd3fc7c747f5e7828f2e62c
    	new file:   .phpintel/ea4602da5cae92046a4f6cbacd6ba97f
    	new file:   .phpintel/f25022fa6ebb51da3ad6b164ee590ed7
    	new file:   .phpintel/index
    	modified:   app/Http/Controllers/FrontController.php
    	new file:   resources/views/conexion_VBack.blade.php
    	renamed:    resources/views/conexion.blade.php -> resources/views/contact_VBack.blade.php
    	new file:   resources/views/legalMention_VBack.blade.php
    	new file:   resources/views/lycee_VBack.blade.php
    	new file:   resources/views/post_VBack.blade.php
    	new file:   resources/views/posts_VBack.blade.php
    	modified:   routes/web.php
    	modified:   storage/logs/laravel.log

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class FrontController extends Controller
 {
@@ -13,7 +14,9 @@ class FrontController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        //return view('home', ['posts' => $posts, 'students'=> $students, 'categories' => $categories]);
+        return view('posts_VBack', compact('posts', 'students'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('conexion_VBack');
+});
+
+Route::get('/mention', function () {
+    return view('legalMention_VBack');
+});
+
+Route::get('/lycee', function () {
+    return view('lycee_VBack');
+});
+
+Route::get('/posts',  'FrontController@index');
+
+Route::get('/post', function () {
+    return view('post_VBack');
+});
+
+Route::get('/contact', function () {
+    return view('contact_VBack');
+});
+
